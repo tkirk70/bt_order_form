242eda41c7f89c8f473d41e8b0d0a32c206bc6fc
TDS <tds@tcg3pl.com>
2024-04-24 12:51:28 -0400

    formatting currency.

--- bt_order_form.py
+++ bt_order_form.py
@@ -49,6 +49,6 @@ filtered_df = df_items[
     (df_items['SIZE'] == size)
 ]
 filtered_df['QTY'] = qty
-filtered_df['Total'] = f'${filtered_df['MSRP'] * qty:.2f}'
+filtered_df['Total'] = f'${filtered_df["MSRP"] * qty:.2f}'
 st.dataframe(filtered_df, hide_index=True, width=1300)
     
