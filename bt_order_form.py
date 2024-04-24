705cbda8ef653b970ba2cf89f6d591293a03be37
TDS <tds@tcg3pl.com>
2024-04-24 10:20:49 -0400

    Adding order dropdowns and item databases.

--- bt_order_form.py
+++ bt_order_form.py
@@ -14,3 +14,17 @@ st.write(f"Selected Customer: **{selected_cust}**")
 # Filter the DataFrame based on the selected customer
 selected_row = df_cust[df_cust['Company'] == selected_cust]
 st.dataframe(selected_row, hide_index=True)
+
+# order input dropdowns
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    style = st.text_input("Style", 'KBT7011')
+    
+with col2:
+    color = st.text_input("Color", 'NONE')
+    
+with col3:
+    size = st.text_input("Size", 'ONESIZE')
+    
