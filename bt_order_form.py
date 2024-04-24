0836138997605721ec6e42ba3cc6ed92f1c5bb23
TDS <tds@tcg3pl.com>
2024-04-24 08:55:18 -0400

    Upload .csv for customer data and initial .py file.

---
+++ bt_order_form.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+df_cust = pd.read_csv('Bourbon Trail Customer List.csv')
+df_cust
+
+st.title("Bourbon Trail Order Form")
+
+# Dropdown for product selection
+selected_cust = st.selectbox("Select a customer", df_cust['Company'])
+
+# Display product details
+st.write(f"Selected Customer: **{selected_cust}**")
+st.write(f"Info: {df[df['Company']]")
